279e6a2f8072baf9ad5f564d2f832efde708518d
Woo-Ch <moscops980417@gmail.com>
2026-07-27 17:50:14 +0900

    26.07.27 끝

--- 08_string.py
+++ 08_string.py
@@ -281,3 +281,247 @@ print(str2.endswith("월요일입니다! 여러분은 할 수 있어요! ")) # F
 print(str2.endswith(" 월요일입니다! 여러분은 할 수 있어요!")) # False
 # 문자열이 완전 똑같아야만 True가 출력된다.
 
+# ==================================
+print("=== 값은 객체다 ===")
+
+print(type("잊어먹으면 안돼!!")) # <class 'str'>
+print(len("이렇게 썼죠??")) # 8
+# endswith와 len의 차이는?
+# endswith는 .으로 연결
+  # .으로 연결하는 이런 도구들은 "메서드"
+  # 문자열이나 int, float처럼 특정 자료형(객체) 내부에 포함된 기능
+# len은 . 사용 안함
+  # () -> 함수
+  # len과 같이 개발자가 직접 선언하지 않은 기본 제공 함수 "내장함수"
+ 
+"str".startswith("s")
+# 123.startswith(1)
+# .으로 사용하는 메서드들은 특정 자료형(객체 타입)마다 다름
+# int 자료형의 객체에는 startswith라는 메서드가 없음
+ 
+# print(len(123)) # len 내장함수는 길이를 반환하기 때문에 int 자료형 사용 불가
+
+# ========================
+# 재할당 복습
+
+num = 1
+num = num + 1 # 2
+num += 1 # 3
+# += 은 복합할당연산자
+# 원래 내 자산의 값에 다음 오는 연산자와 값을 적용해서 재할당
+
+# ======================
+print("=== .upper() ===")
+
+str3 = "abcdefg"
+print(str3) # abcdefg
+
+str3.upper # 반환은 대문자인데, 값에 재할당은 X
+print(str3) # abcdefg  # 기존 str3의 값인 소문자를 그대로 출력
+
+# 앞으로 계속 대문자로 변환한 값을 사용하고 싶다면
+# 변수에 재할당
+# 변수 재할당에서 변수 스스로를 부르는 것이 가능
+# 재할당에서 변수 스스로 값을 부르려면 무조건 "재할당"이어야 함
+str3 = str3.upper()
+print(str3) # ABCDEFG
+
+# str4 = str4.upper() # Error
+# 위의 경우 최초 변수 할당 시에는 저장된 값이 없어서
+# 변수 스스로 값을 불러와 할당 불가능
+
+# ==============================
+user_name = "kim chul soo"
+
+# capitalize는 문자열의 첫 글자만 대문자로 변환
+print(user_name.capitalize()) # Kim chul soo
+
+# title은 띄어쓰기 기준으로 각 단어의 첫 글자들을 모두 대문자로 변환
+print(user_name.title()) # Kim Chul Soo
+
+# '를 사용한 경우 경우 다른 단어로 인식
+print("i'm full".title()) # I'M Full
+print("i\'m full".title()) # I'M Full
+
+# ==========================
+print("=== .strip() ===")
+
+# 공백 제거
+# .strip(): 앞과 뒤의 모든 공백 제거 (중간 띄어쓰기는 그대로 유지)
+# .lstrip(): left(왼쪽) 공백만 제거
+# .rstrip(): right(오른쪽) 공백만 제거
+
+raw = "   정상   "
+print(raw.strip()) # "정상"
+print(raw.lstrip()) # "정상   "
+print(raw.rstrip()) # "   정상"
+
+# 문자열의 가운데 공백은 strip으로 지우지 못함
+print("   정   상   ".strip()) # "정   상"
+
+print(raw) # "   정상   "
+# strip은 재할당이나 새 변수에 할당하지 않는 이상 휘발
+
+# strip으로 문자 제거
+str4 = "===정상==="
+print(str4.strip("=")) # 정상
+# 인자로 전달한 양 끝의 =이 모두 제거됨
+
+str5 = "=정상====================="
+print(str5.strip("=")) # 정상
+# 갯수 상관 없이 인자로 전달한 문자를 무조건 삭제
+
+print(str5.strip("= ")) # wjdtkd
+# strip 자체가 공백을 지우는 것이기 때문에
+# 공백 상관없이 양 끝의 해당 문자열 삭제
+
+str6 = "==정==상===="
+print(str6.strip("=")) # 정==상
+# 글자 중간에 있는 문자열은 건드리지 않음
+
+# ==============================
+print("=== 체이닝 ===")
+
+raw = "    NORMAL    "
+# 체이닝 X
+step1 = raw.strip() # "NORMAL"
+step2 = step1.lower() # "normal"
+
+# 체이닝 X, 기존 변수에 재할당
+raw = raw.strip() # "NORMAL"
+raw = raw.lower() # "normal"
+
+# 체이닝 O
+chain = raw.strip().lower() # "normal"
+
+# 기존 변수에 재할당도 가능
+# raw = rwa.strip().lower()
+
+# 변수에 할당하지 않고 사용 가능
+# print(raw.strip().lower()) # "normal"
+
+# strip() 메서드에 인자로 들어가는 문자열은 완전히 동일하지 않아도 전부 삭제
+str8 = "aabb 이렇게? cd"
+print(str8.strip("abcd ")) # "이렇게?"
+print(str8.strip("bc")) # "aabb 이렇게? cd" # 중간에 있는 건 제거 불가
+
+#===============================
+print("=== replace() ===")
+
+# 특정 문자열을 제거하거나 치환할 때 사용
+# 제거할 때에는 인자의 두 번째를 ""(빈문자열)로 작성
+print("정 상 가 동".replace(" ", "")) # 정상가동 (중간 공백 제거)
+print("      정              상         가   동   ".replace(" ", "")) # 정상가동
+
+# 글자 치환
+print("고장".replace("고장", "fault")) # falut
+print("고장".replace("고", "fault")) # falut장
+
+# 단어 치환
+str9 = "설비 정상 가동"
+print(str9.replace("정상", "점검")) # 설비 점검 가동
+
+# replace() 체이닝
+num = "    010-1234-1234    "
+num = num.replace(" ", "").replace("-", "") # 01012341234
+print(num)
+
+# ==============================
+print("=== split() ===")
+
+# 문자열 자르기
+# 결고는 대괄호에 감싸진 "리스트" 자료형
+# 리스트는 순서가 있기 때문에 왼쪽에서부터 0으로 시작하는 인덱스가 자동 생성
+
+drinks = "에스프레소 아메리카노 카페라떼"
+print(drinks.split()) # ['에스프레소', '아메리카노', '카페라떼']
+# 띄어쓰기를 기준으로 나뉘어진 세 개의 문자열을 대괄호에 감싸서 반환
+
+# 구분자를 특정하고 싶은 경우
+fruits = "딸기,거봉,키위,사쿠란보"
+print(fruits.split(",")) # ['딸기', '거봉', '키위', '사쿠란보']
+# 무낮열 콤마를 기준으로 분할
+
+fruits2 = "딸기, 거봉, 키위, 사쿠란보"
+print(fruits2.split(",")) # ['딸기', ' 거봉', ' 키위', ' 사쿠란보']
+# 기준을 콤마로 했기 때문에 공백 그대로 
+
+print(fruits2.split(", ")) # ['딸기', '거봉', '키위', '사쿠란보']
+# 문자열 콤마+공백 1칸을 기준으로 분할됨
+
+# 리스트의 인덱스
+fruits_list = fruits.split(",")
+print(fruits_list) # ['딸기', '거봉', '키위', '사쿠란보']
+
+# 거봉만 출력하기
+print(fruits_list[1]) # 거봉
+# 출력하고자 하는 요소의 인덱스를 대괄호로 감싸서 호출
+print(fruits_list[-1]) # 사쿠란보
+
+# split 횟수 제한
+num = "010-1234-1234"
+# ["010", "1234-1234"]
+print(num.split("-", 1)) # ['010', '1234-1234']
+
+# ========================
+print("=== join() ===")
+# 리스트를 하나의 문자열로 합침
+# "구분자".join(리스트)
+# 모든 요소가 합쳐져서 하나의 문자열로 반환
+
+fruits_list = ['딸기', '거봉', '키위', '사쿠란보']
+
+"-".join(fruits_list) # "딸기-거봉-키위-사쿠란보"
+",".join(fruits_list) # "딸기,거봉,키위,사쿠란보"
+", ".join(fruits_list) # "딸기, 거봉, 키위, 사쿠란보"
+
+# 실습. python 출력하기 - 선생님 방법
+
+word = "python"
+
+# 방법1. strip + capitalize
+print(word[:2] + word.strip("py").capitalize()) # pyThon
+
+# 방법2. replace 사용
+print(word.replace("t","T")) # pyThon
+
+# 방법3. 슬라이싱 + T만 upper 사용
+print(word[:2] + word[2].upper() + word[3:]) # pyThon
+      
+# 방법4. 인덱싱으로 글자 하나씩 연결
+print(word[0] + word[1] + word[2].upper() + word[3] + word[4] + word[5])  # pyThon
+
+# 방법5. 인덱싱 + strip + title
+print(word[:2] + word.strip("py").title()) # pyThon
+
+# 방법6. split + join
+print(word.split("t")) # ['py', 'hon']
+print("T".join(word.split("t"))) # pyThon
+print(word[2].upper().join(word.split("t"))) # pyThon
+
+# ==========================
+print("=== print 함수의 sep, end ===")
+
+print("2026", "07", "27") # 2026 07 27 (기본적으로는 공백 1칸)
+
+# sep 속성을 사용하면 구분을 공백이 아닌 특정 문자열로 가능
+print("2026", "07", "27", sep="사랑해") # 2026사랑해07사랑해27
+# 공백 대신 sep 속성에 전달한 문자열이 사빕되어 이어짐
+
+print("안녕", "하세") # 안녕 하세
+print("안녕", "하세", end="\n") # 안녕 하세요
+# end 속성 사용 시 출력문 마지막에 해당 문자열이 붙어 삽입
+
+# print("안녕", "하세", end="요", "ㅎㅎ") # Error
+# end 속성 뒤에 또 인자 넘기기 불가
+
+# print 함수 + 사용 시 sep과 end
+print("안녕", "하세", end = "요\n" + "이렇게?") # 안녕 하세요이렇게?
+# 정상 동작은 하나, 사용 자제^^ (end를 썼는데 굳이 끝에 +를 사용해 붙이는건 노이해)
+
+# 기본적으로 print문에는 sep으로 공백 한 칸,
+# end로 \n(줄바꿈)이 적용되어 있음
+# 근데, 개발자가 각 속성을 직접 부여할 경우
+# 기본값이 아닌 전달받은 속성값을 사용
+print("이런식으로 쓰죠?", "근데 안보이는 기본값이 있어요", sep = " ", end = "\n")
+
